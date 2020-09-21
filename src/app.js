ee03678cda4e86306bcd5187246820c6bffefadb
joseph-zabaleta <joseph.l.zabaleta@gmail.com>
2020-09-21 15:51:14 -0700

    Converted todo list and app over to functions

--- src/app.js
+++ src/app.js
@@ -3,18 +3,6 @@ import React from 'react';
 import ToDo from './components/todo/todo.js';
 // import Header from './components/header/header.js';
 
-
-// export default class App extends React.Component {
-//     render() {
-//         return (
-//             <>
-//                 <Header />
-//                 <ToDo />
-//             </>
-//         );
-//     }
-// }
-
 export default function App() {
 
     return (
