f9055c12bd32da24f311e87545ebbc1fb20eae08
joseph-zabaleta <joseph.l.zabaleta@gmail.com>
2020-09-21 09:48:25 -0700

    Added proprosed Application Structure

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import ToDo from './components/todo/todo.js';
-import Header from './components/header.js';
+import Header from './components/header/header.js';
 
 
 export default class App extends React.Component {
