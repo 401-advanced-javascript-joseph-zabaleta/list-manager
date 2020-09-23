8ef3b4cf48f6720905d0bef553a81c59999096d3
joseph-zabaleta <joseph.l.zabaleta@gmail.com>
2020-09-23 14:58:28 -0700

    Updated header component styling, as well as list styling

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,18 +1,28 @@
 import React from 'react'
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
+import Form from 'react-bootstrap/Form'
+import FormControl from 'react-bootstrap/FormControl';
+import Button from 'react-bootstrap/Button';
+
+import './header.scss'
 
 export default props => (
 
     <header>
 
         <Navbar bg="primary" variant="dark">
-
             <Nav className="mr-auto">
-
                 <Nav.Link>Home</Nav.Link>
-
             </Nav>
+
+            <Form inline>
+                <FormControl type='text' placeholder='Username' className='mr-sm-2' />
+                <FormControl type='text' placeholder='Password' className='mr-sm-2' />
+                <Button variant='dark'>
+                    Login
+                </Button>
+            </Form>
 
         </Navbar>
 
--- src/components/todo/list.js
+++ src/components/todo/list.js
@@ -1,18 +1,51 @@
 import React from 'react';
-import ListGroup from 'react-bootstrap/ListGroup'
+import ListGroup from 'react-bootstrap/ListGroup';
+import Card from 'react-bootstrap/Card';
+import Badge from 'react-bootstrap/Badge';
+
+
+import './todo.scss';
 
 
 export default function TodoList(props) {
+
+    let style = {
+
+        'margin-bottom': '25px',
+        'box-shadow': '5px 5px 12px 0px grey',
+
+    }
 
     return (
 
         <ListGroup>
             {props.list.map(item => (
-                <ListGroup.Item variant={item.complete ? 'success' : 'danger'} key={item._id}>
-                    <span onClick={() => props.handleComplete(item._id)}>
-                        {item.text}
-                    </span>
-                </ListGroup.Item>
+                // <ListGroup.Item variant={item.complete ? 'success' : 'danger'} key={item._id}>
+                //     <span onClick={() => props.handleComplete(item._id)}>
+                //         {item.text}
+                //     </span>
+                // </ListGroup.Item>
+                <Card style={style}>
+                    <Card.Header>
+                        <span onClick={() => props.handleComplete(item._id)}>
+                            <Badge pill variant={item.complete ? 'success' : 'warning'}>
+                                {item.complete ? 'Complete' : 'Pending'}
+                            </Badge>
+                        </span>
+                        <span>
+                            {item.assignee}
+                        </span>
+                    </Card.Header>
+                    <Card.Body>
+                        <Card.Text>
+                            {item.text}
+                        </Card.Text>
+                        <Card.Text className='text-right'>
+                            Difficulty: {item.difficulty}
+                        </Card.Text>
+
+                    </Card.Body>
+                </Card>
             ))}
         </ListGroup>
 
